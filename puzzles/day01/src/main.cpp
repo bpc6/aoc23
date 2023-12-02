4b41b117b9b435d71e972b64618762dd2d1df9ec
Bryan Covell <bpc6@vt.edu>
2023-12-02 14:04:42 -0500

    finished 1.2

--- puzzles/day01/src/main.cpp
+++ puzzles/day01/src/main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+#include <algorithm>
 #include <fstream>
-#include <vector>
+#include <iostream>
 #include <string>
+#include <vector>
 
 std::vector<std::string> readLines(const std::string& file_path) {
   std::ifstream file(file_path);
@@ -19,33 +20,41 @@ std::vector<std::string> readLines(const std::string& file_path) {
   return lines;
 }
 
+const std::vector<std::string> INT_STRINGS = {"one", "two",   "three", "four", "five",
+                                              "six", "seven", "eight", "nine"};
 
-int firstNum(const std::string& s) {
-  for (const char& c : s) {
-    if (isdigit(c)) {
-      return c - '0';
+int firstNumOrText(const std::string& s, const std::vector<std::string>& int_strings) {
+  for (std::string::size_type i = 0; i < s.size(); ++i) {
+    if (isdigit(s[i])) {
+      return s[i] - '0';
+    }
+    std::string substring = s.substr(i);
+    for (int j = 0; j < int_strings.size(); ++j) {
+      if (substring.rfind(int_strings[j], 0) == 0) {
+        return j + 1;
+      }
     }
   }
-  throw std::runtime_error("No number found");
 }
 
-int lastNum(const std::string& s) {
-  for (std::string::const_reverse_iterator it = s.rbegin(); it != s.rend(); ++it) {
-    if (isdigit(*it)) {
-      return *it - '0';
-    }
+int lastNumOrText(std::string s, std::vector<std::string> int_strings) {
+  for (auto& int_string : int_strings) {
+    std::reverse(int_string.begin(), int_string.end());
   }
+  reverse(s.begin(), s.end());
+  return firstNumOrText(s, int_strings);
 }
 
-
-int main() {
-  std::string file_path = "input/input.txt";
-  std::vector<std::string> lines = readLines(file_path);
-
+int day01(const std::vector<std::string>& lines) {
   int sum = 0;
   for (const auto& l : lines) {
-    sum += firstNum(l) * 10 + lastNum(l);
+    sum += firstNumOrText(l, INT_STRINGS) * 10 + lastNumOrText(l, INT_STRINGS);
   }
   std::cout << sum << std::endl;
+  return EXIT_SUCCESS;
+}
+
+int main() {
+  day01(readLines("input/input.txt"));
   return EXIT_SUCCESS;
 }
