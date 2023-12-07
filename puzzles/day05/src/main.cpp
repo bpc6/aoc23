9c24abbe4b59ba6291e7f4e0e5f6e0d4b9fca643
Bryan Covell <bpc6@vt.edu>
2023-12-07 08:37:34 -0500

    day5 setup

---
+++ puzzles/day05/src/main.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include "utils.h"
+
+int main() {
+  std::vector<std::string> lines = readLines("input/test.txt");
+  std::cout << "hello" << std::endl;
+}
